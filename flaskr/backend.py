3aadee3c72e7dcb809e0e28ba96e53907486003b
Samuel Lopez <sam@techexchange.in>
2023-03-02 00:01:21 +0000

    Implemented instance of the upload bucket and blob of the upload bucket

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,8 +1,14 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
+
 class Backend:
 
     def __init__(self):
-        pass
+        # Instantiate a new client for Cloud Storage
+        self.client = storage.Client()
+
+        # Get a reference to the GCS bucket
+        self.bucket = self.client.bucket('web-uploads')
         
     def get_wiki_page(self, name):
         pass
@@ -10,8 +16,11 @@ class Backend:
     def get_all_page_names(self):
         pass
 
-    def upload(self):
-        pass
+    def upload(self, file):
+        # Create a new blob in the bucket and upload the file data
+        blob = self.bucket.blob(file.filename)
+        blob.upload_from_file(file)
+        
 
     def sign_up(self):
         pass
