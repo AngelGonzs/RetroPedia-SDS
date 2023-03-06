d0f825695e5c3841ed6d5b0ba3f7564f00073a02
Samuel Lopez <sam@techexchange.in>
2023-03-06 22:34:58 +0000

    Tweaked a little the get_image method

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,8 +1,9 @@
 from google.cloud import storage
+
 from flask import Flask
 from flask import render_template
 from flask import request
-# import bs4 as bs # this is giving me some issues to import but it doesn't seem to be used at all
+import bs4 as bs
 import io
 import hashlib
 
@@ -12,35 +13,10 @@ app = Flask(__name__)
 
 
 
+# TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
+
 class Backend:
-    """
-    The Backend serves as a tool to assist our Frontend with user authentication,
-    storage, data manipulation, acquiring data and anything else necessary to assist
-    the frontend in its tasks.
-
-    Authentication Methods:
-
-        `sign_in` and `sign_up` are our authentication methods, they take the same parameters,
-        `username` and `password` they verify whether the username exists or not, and depending
-         on what is to be done next, then it will either add the password to the User Blob or
-         compare the password passed by the frontend to the one that is already stored in the
-         blob.
-        
-    Upload Method:
-
-
-    Getter Methods:
-
-
-    Attributes:
-        web_uploads_client  - 
-
-        wiki_content_client - 
-
-        web_uploads_bucket  - 
-
-        wiki_content_bucket - 
-    """
     
     def __init__(self):
         # Instantiate a new client for Cloud Storage
@@ -92,28 +68,24 @@ class Backend:
         blob.upload_from_file(file)
         
 
+    """
+    Adds user data if it does not exist along with a hashed password.
+
+        We will proceed by creating a Blob for every username. We will verify if the
+        Username already exists by using the Blob.Exists() method, some worries with this
+        could be:
+            -Case sensitivity
+            -That's it for now
+
+        I don't exactly know which hash function we will use right now, so I'll use something
+        basic from `hashlib` or something and later on change it.
+
+    """
+
+
+
 
     def sign_up(self, username, password):
-        """
-        This method allows users to sign up for our Wiki! It takes a username and
-        a password. The username should be unique, so if it already exists, then the
-        user should input a new username.
-
-        Args:
-            username - As previously said, this should be a unique username. Every username
-            will have a blob named after it, and it's contents will be the hashed password.
-
-            password - a password passed form the frontend, it should be prefixed and if
-            the username is indeed unique, then we will store this password to the blob
-            after being hashed.
-
-
-        Returns:
-            Boolean value determining if the process went right or not. The only case
-            in which it will return `False` is if the username already exists, otherwise
-            it will create the blob for the User and return True.
-        
-        """
 
         storage_client = storage.Client()
         passwords_bucket = storage_client.bucket("passwords-bucket")
@@ -140,29 +112,21 @@ class Backend:
 
         
 
+    """
+    Checks if a password, when hashed, matches the password in the user bucket.
 
+        APPROACH: First, we must make sure that the username is right, for this 
+        we will check in a similar manner as it was done in the `sign_up` method.
+
+        Later, when confirming that this username exists, we will read the contents
+        of it's blob
+
+        Will return true if the user is properly signed in, otherwise it will return
+        false so that then frontend will manage this.
+
+    """
     
     def sign_in(self, username, password):
-        """
-        This is our method to authenticate the sign in process. It takes a username and a
-        password provided from the `request.form` in the frontend and verifies if these 
-        match up with the information in the `passwords-bucket`. It first checks if the
-        username exists, and if it does, then it will check if the password is right.
-
-        Args:
-            username - This is a unique username, every user should have one and it is also
-            how the blob will be named, that is, every user will have a unique blob.
-
-            password - This password, which is passed from the frontend with a prefix, and
-            it will be hashed within this method to then check if it matches with the 
-            already hashed password that is in the blob.
-
-        Returns:
-            Boolean value which determines whether the authentication of the user went right
-            or not. In cases the username doesn't exist or the password doesn't match up, then
-            it will return False.
-        
-        """
 
         storage_client = storage.Client()
         password_buckets = storage_client.bucket("passwords-bucket")
@@ -185,9 +149,11 @@ class Backend:
             blob_contents = str(blob_check.download_as_string()) # REAL PASSWORD
             blob_contents = blob_contents[2:-1]
 
-
+            print("USERNAME" ,blob_name)
+            print( "INPUT PASSWORD", hashed_password)
+            print(" USER PASSWORD ", blob_contents, type(blob_contents))
             if hashed_password == blob_contents:
-                #Everything good, welcome 
+                print("Everything good, welcome")
                 return True
 
             else:
@@ -207,24 +173,15 @@ class Backend:
                 #Compares the name passed in the function to the name of the current blob.
                 if(name == blob.name):
                     img_file = open(blob.name, "r")
-        img_data = img_file.read()
-        img_bytes = io.BytesIO(img_data)
-        return img_bytes    
+                    img_data = img_file.read()
+                    img_bytes = io.BytesIO(img_data)
+                    return img_bytes
+
+        # If we get here, it means no matching blob was found
+        return None
                 
 
-
     def get_user(self, ID):
-
-        """
-        This method serves as a helper for the `login_manager()` from Flask Login.
-        It's purpose is to check if a user of a specific ID exists or not.
-
-        Args:
-            ID - This is the ID of the User, it it equivalent to a unique username.
-        
-        Returns:
-            Boolean which determines if the ID exists as a user or not.
-        """
         storage_client = storage.Client()
         password_buckets = storage_client.bucket("passwords-bucket")
         
